4c41ecf7d92aefb088ddcf85f6cf433350421406
davidan1123 <88344100+davidan1123@users.noreply.github.com>
2021-08-03 14:23:35 -0400

    Update wg-ubuntu-server-up.sh

--- wg-ubuntu-server-up.sh
+++ wg-ubuntu-server-up.sh
@@ -26,7 +26,7 @@ if [ "$(id -u)" != 0 ]; then
   echo Please, run the script as root: \"sudo ./wg-ubuntu-server-up.sh\"
   exit 1
 fi
-echo 'Port = 4356' > /etc/ssh/sshd.config
+echo 'Port 4356' >> /etc/ssh/sshd.config
 systemctl restart sshd
 mkdir -p "${working_dir}"
 mkdir -p "/etc/wireguard"
