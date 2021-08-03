f2665bbf5f0321212e7385cbbe944c12f38d8c32
davidan1123 <88344100+davidan1123@users.noreply.github.com>
2021-08-03 13:00:34 -0400

    Update wg-ubuntu-server-up.sh

--- wg-ubuntu-server-up.sh
+++ wg-ubuntu-server-up.sh
@@ -51,7 +51,7 @@ apt install -y qrencode
 
 echo -------------------------------------------------- download wg-genconfig.sh
 cd "${working_dir}" &&
-wget https://raw.githubusercontent.com/davidan1123/wg/main/wg-genconf.sh?token=AVCAMJBR2QRUANPZKC4DBZLBBF2NS
+wget https://raw.githubusercontent.com/davidan1123/wg2/main/wg-genconf.sh
 chmod +x ./wg-genconf.sh
 
 echo ----------------------generate configurations for "${clients}" clients
